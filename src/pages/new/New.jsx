92847a476a27120c29c6b1e939557c23d6c37198
Krnandi <ifanifan791@gmail.com>
2024-06-23 15:43:03 +0700

    ubah teks di app

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -2,10 +2,58 @@ import "./new.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
-import { useState } from "react";
+import { useState } from "react"; 
+import { doc, serverTimestamp, setDoc, addDoc, collection } from "firebase/firestore";
+import { auth, db } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth"; 
+import { useNavigate, useLocation } from "react-router-dom";
 
 const New = ({ inputs, title }) => {
-	const [file, setFile] = useState("");
+  const [file, setFile] = useState("");  
+  const [data, setData] = useState({});
+
+  const navigate = useNavigate();
+  
+  const location = useLocation(); 
+  const type = location.pathname.split('/')[1]; 
+
+  //console.log(data);
+
+  const handleInput = (e) => {
+    const id = e.target.id;
+    const value = e.target.value;
+
+    setData({ ...data, [id]: value });
+  };
+
+  const handleAdd = async (e) => {
+    e.preventDefault();
+    try {
+      switch (type) {
+        case "users":
+          const res = await createUserWithEmailAndPassword(
+            auth,
+            data.email,
+            data.password
+          );
+          await setDoc(doc(db, type, res.user.uid), {
+            ...data,
+            timeStamp: serverTimestamp(),
+          });
+          break; 
+        default:
+          await addDoc(collection(db, type), {
+            ...data,
+            timeStamp: serverTimestamp(),
+          });
+          break;
+      } 
+      
+      navigate(-1)
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div className="new">
@@ -16,40 +64,43 @@ const New = ({ inputs, title }) => {
           <h1>{title}</h1>
         </div>
         <div className="bottom">
-        <div className="left">
-  <img
-    src={
-      file
-        ? URL.createObjectURL(file)
-        : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
-    }
-    alt=""
-  />
-</div>
-          
-<div className="right">
-  <form>
-    <div className="formInput">
-      <label htmlFor="file">
-        Image: <DriveFolderUploadOutlinedIcon className="icon" />
-      </label>
-      <input
-        type="file"
-        id="file"
-        onChange={(e) => setFile(e.target.files[0])}
-        style={{ display: "none" }}
-      />
-    </div>
+          <div className="left">
+            <img
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div className="right">
+            <form onSubmit={handleAdd}>
+              <div className="formInput">
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
+              </div>
 
-    {inputs.map((input) => (
-      <div className="formInput" key={input.id}>
-        <label>{input.label}</label>
-        <input type={input.type} placeholder={input.placeholder} />
-      </div>
-    ))}
-    <button>Send</button>
-  </form>
-</div>
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}</label>
+                  <input 
+                  type={input.type} 
+                  placeholder={input.placeholder} 
+                  id={input.id} 
+                  onChange={handleInput} />
+                </div>
+              ))}
+              <button>Send</button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
