c3d03c64b47ca5454147751cde658da76643ee04
Krnandi <ifanifan791@gmail.com>
2024-06-25 13:41:57 +0700

    ubah teks di app

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -3,8 +3,13 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
 import List from "../../components/datatable/Datatable";
+import Datatable from "../../components/datatable/Datatable";
+import { useLocation } from "react-router-dom";
 
 const Single = () => {
+  const location = useLocation();
+  const id = location.pathname.split('/')[2];
+
   return (
     <div className="single">
       <Sidebar />
@@ -23,6 +28,10 @@ const Single = () => {
               />
               <div className="details">
                 <h1 className="itemTitle">Jane Doe</h1>
+                <div className="detailItem">
+                  <span className="itemKey">ID:</span>
+                  <span className="itemValue">{id}</span>
+                </div>
                 <div className="detailItem">
                   <span className="itemKey">Email:</span>
                   <span className="itemValue">janedoe@gmail.com</span>
@@ -54,7 +63,7 @@ const Single = () => {
         <div className="bottom">
         <div className="bottom">
           <h1 className="title">Last Transactions</h1>
-          <List />
+          <Datatable columns={columns} />
         </div>
         </div>
       </div>
